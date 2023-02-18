95182b9d28debdbb2ddf4c6954d4a0fa7248d7ae
Đỗ Huy Hoàng <dohuyhoang.se.dev@gmail.com>
2023-02-18 21:44:00 +0700

    OC-49: api create new channel

--- message-service/src/main.ts
+++ message-service/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({ origin: '*' });
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
