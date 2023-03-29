99f3b33c5d3e37496105c8ea84b4f5a9010f9408
Đỗ Huy Hoàng <dohuyhoang.se.dev@gmail.com>
2023-03-29 17:49:51 +0700

    update workspace and ci

--- workspace-service/src/main/java/com/octopus/workspaceservice/security/config/WebSecurityConfig.java
+++ workspace-service/src/main/java/com/octopus/workspaceservice/security/config/WebSecurityConfig.java
@@ -1,19 +1,19 @@
 package com.octopus.workspaceservice.security.config;
 
-import com.octopus.authservice.security.UserDetailsServiceImpl;
-import com.octopus.authservice.security.jwt.JwtConfigurer;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.octopus.authutils.jwt.JWTConfig;
+import com.octopus.authutils.jwt.JWTFilter;
+import com.octopus.authutils.jwt.JWTUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,28 +21,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
-    private final JwtConfigurer jwtConfigurer;
-    
-    private final UserDetailsServiceImpl userDetailService;
-    
-    @Autowired
-    public WebSecurityConfig(JwtConfigurer jwtConfigurer, UserDetailsServiceImpl userDetailServiceImpl) {
-        this.jwtConfigurer = jwtConfigurer;
-        this.userDetailService = userDetailServiceImpl;
-    }
-    
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
-    }
-    
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-    
+
+    private static final String[] SWAGGER_AUTH_WHITELIST = {
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
+    };
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -53,21 +39,47 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             }
         };
     }
-    
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests().and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeRequests()
-                .antMatchers("/workspace/swagger-ui.html", "/workspace/swagger-ui/**", "/workspace/api-docs/**").permitAll()
-                .antMatchers("/actuator/health").permitAll()
-                .antMatchers("/api/workspace/**").permitAll()
-                .anyRequest().authenticated()
-                .and().apply(jwtConfigurer);
+        // @formatter:off
+
+        http
+            .cors()
+        .and()
+            .csrf()
+            .disable()
+            .httpBasic()
+            .disable()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+            .authorizeRequests()
+            .antMatchers(SWAGGER_AUTH_WHITELIST).permitAll()
+            .anyRequest()
+            .authenticated()
+        .and()
+            .exceptionHandling()
+            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            .accessDeniedHandler(new AccessDeniedHandlerImpl())
+            .and()
+            .addFilterAfter(new JWTFilter(jwtUtils()), UsernamePasswordAuthenticationFilter.class);
+
+        // @formatter:on
     }
-    
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(8);
+    public JWTFilter jwtFilter() {
+        return new JWTFilter(jwtUtils());
+    }
+
+    @Bean
+    public JWTUtils jwtUtils() {
+        return new JWTUtils(jwtConfig());
+    }
+
+    @Bean
+    public JWTConfig jwtConfig() {
+        return new JWTConfig();
     }
 }
